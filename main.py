895eb5ef5d16bc51bb1ba7114217b6ab3ed7eeb2
martingkc <104863799+martingkc@users.noreply.github.com>
2022-11-12 13:36:21 +0100

    duzenlemeler

--- main.py
+++ main.py
@@ -1,64 +1,95 @@
-import time, base64, hmac, hashlib, requests, json
+import time, requests, json
 from collections import defaultdict
-import time, base64, hmac, hashlib, requests, json
 import matplotlib.pyplot as plt
 import pandas as pd
-def getklinedata(name, interval, Ninterval):
+# olcumleri to = su anki zaman ve from = to - olcumsuresi(dk)*60(s)*olcumsayisi olarak aliyorum 
+#bu parametreler sonra url'yi olusturmak icin kullaniliyor 
+#orn: total hesabinda from = to - 60*60*1 60dklikolcumler*60s*1olcum
+#olcumlerden alinan dataframeler x axisi uzerinde concatenate ediliyor (axis =1)  pd.concat([df, df2], axis=1) 
+#pricexamount degeri fiyatin acilis ve kapanis degerlerinin aritmetik ortalamasina gore hesaplaniyor ort. fiyat = (open + close)/2 // ortfiyat*volum
+#degerleri try karsiliginda olan ve statusu trading olan coinler key'i ismi olan ve degeri total olan bir dictionary'ye 
+#kaydediliyor sonrasinda bir lambda function ile degerlerine gore siralandiriliyorlar
+#her iterasyonda en pricexammount degeri en yuksek olan 20 coinin isimleri numlab arrayine kaydediliyor sonrasinda bu array graph'in legendini olusturmak icin kullaniliyor
+#graphler otomatik olarak filein bulundugu klasore t20-(sayi).png olarak kaydediliyorlar
+#programin tekrarlama suresi sleeptime variableinin degerini degistirerek ayarlanabilir
+#(eger deger dusuk olursa btcturkun ddos korumasi siteye erisimi kisitliyor boyle bir durumda iki dk sonra tekrardan denemek gerekiyor) 
+# calistirmadan once >pip install matplotlib ve >pip install pandas komutlarini kullanarak bu iki packagei indirmek gerekiyor 
+
+
+
+def total(name):
     base = "https://graph-api.btcturk.com"
     to = int(time.time())
-    From = to-60*interval*Ninterval
+    interval = 60
+    intervalN = 1
+    From = to-60*interval*intervalN
+    
     method = "/v1/klines/history?from="+str(From) + "&resolution="+str(interval)+"&symbol="+name+"&to="+ str(to)
     uri = base + method
     result = requests.get(uri)
-    
-    return result
-
-def total(name):
-    result = getklinedata(name, 60, 1)
     result = json.loads(result.text)
     return (int(((result['o'][0]+result['c'][0])/2)*result['v'][0]))
 
 def getdf(name): 
-    result = getklinedata(name, 5, 20)
+    base = "https://graph-api.btcturk.com"
+    to = int(time.time())
+    interval = 5
+    intervalN = 19
+    From = to-60*interval*intervalN
+    method = "/v1/klines/history?from="+str(From) + "&resolution="+str(interval)+"&symbol="+name+"&to="+ str(to)
+    uri = base + method
+    result = requests.get(uri)
     df = pd.read_json(result.text)
     
     return df
     
    
 
-base = "https://api.btcturk.com"
-method = "/api/v2/server/exchangeinfo"
-uri = base+method
 
+sleeptime = 60*60 
 count = 0
 while(True):
+    namelab = []
+    base = "https://api.btcturk.com"
+    method = "/api/v2/server/exchangeinfo"
+    uri = base+method
     names = defaultdict(list)
     plt.style.use('ggplot')
     names.clear()
-    result = requests.get(uri)
+    result = requests.get(url =uri)
     result = json.loads(result.text)
     coins = result['data']
     for i in coins['symbols']:
         try:
             name = i['name']
-            if i['status']=='TRADING':
-                
-                names[name].append(int(total(name)))
+            if i['status']=='TRADING' and name[len(name)-1] == 'Y':
+                names[name].append(total(name))
                
-        except: 
+        except:
             print('VAL NA')
     names = sorted(names.items(), key=lambda x: x[1], reverse=True)   
     print(names)
+    namelab.append(names[0][0])
+    df = getdf(names[0][0])
     
-    for i in range(20): 
+    df = ((df['o']+df['c'])/2)*df['v']
+    
+    for i in range(1,20): 
         df2 =getdf(names[i][0])
-        df2 = ((df2['o']+df2['c'])/2)*df2['v'] #OCLHS'de total degerinin ortalama satis ucreti * volum olarak alindigini gordum o yuzden kapanma ve acilma fiyatlarinin basit bir ortalamasini alarak ilerledim 
-        df2.plot(label=names[i][0])
-    plt.legend()
+        namelab.append(names[i][0])
+        df2 = ((df2['o']+df2['c'])/2)*df2['v']
+        df = pd.concat([df, df2], axis=1)
+        #OCLHS'de total degerinin ortalama satis ucreti * volum olarak alindigini gordum o yuzden kapanma ve acilma fiyatlarinin basit bir ortalamasini alarak ilerledim 
+    print(df) 
+    
+    plt.figure(figsize=(11,11))
+    lines = plt.plot( df)
+    plt.legend(iter(lines), namelab,loc='upper right')
+    
+    
     plt.grid()
-    plt.title("BTCTURK T-20 priceXamount")
-    fname = 't20'+str(count)+'.png'
-    plt.savefig(fname)
+    plt.title("BTCTURK T-20 priceXamount TRY")
+    fname = 't20-'+str(count)+'.png'
+    plt.savefig(fname, dpi=100)
     count += 1
-    time.sleep(60*60)
-
+    time.sleep(sleeptime)
